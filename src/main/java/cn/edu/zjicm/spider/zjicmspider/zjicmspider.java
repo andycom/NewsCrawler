72a9cf75e11761912e5a8bd06329938325bf2a73
Andy <hzwxuex@tairanchina.com>
2017-04-24 10:23:18 +0800

    add code
    Signed-off-by: Andy <hzwxuex@tairanchina.com>

--- src/main/java/cn/edu/zjicm/spider/zjicmspider/zjicmspider.java
+++ src/main/java/cn/edu/zjicm/spider/zjicmspider/zjicmspider.java
@@ -1,25 +1,20 @@
 package cn.edu.zjicm.spider.zjicmspider;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
-import org.apache.logging.log4j.Logger;
-
-import cn.edu.zjicm.spider.core.zjicmSpider;
+import org.slf4j.LoggerFactory;
 
 public class zjicmspider {
 
 	public static void main(String[] args) throws Exception {
-
+		org.slf4j.Logger logger = LoggerFactory.getLogger("");
 		ArrayList<URL> seedUrls = new ArrayList<URL>();
 		String taskName = "zjicm0813";
 		int deepth = 5;
-		PropertyConfigurator.configure("src/log4j.properties");
-		zjicmspider.logger = Logger.getLogger(zjicmspider.class);
 		// zjicmSpider spider = new zjicmSpider(seedUrls, taskName,deepth); //
 
-		zjicmspider.logger.info("start " + taskName + "\tURLS" + seedUrls);
+		logger.info("start " + taskName + "\tURLS" + seedUrls);
 		// spider.start();
 
 	}
